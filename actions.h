d544da12e03f369965ed5c31a2b534df0a5b98fe
Sara <sarah.oulmaki@gmail.com>
2026-04-03 17:49:14 +0000

    Actions -> Enum

--- actions.h
+++ actions.h
@@ -1,37 +1,33 @@
 #ifndef ACTIONS_H
 #define ACTIONS_H
 
-// On renvoi une de ces actions a chaque tour
+// Actions possibles a chaque tour.
+typedef enum {
+	ACTION_MOVE_L = 1,
+	ACTION_MOVE_R = 2,
+	ACTION_MOVE_U = 3,
+	ACTION_MOVE_D = 4,
 
-// Deplacements simples (1 case).
-#define ACTION_MOVE_L     1
-#define ACTION_MOVE_R     2
-#define ACTION_MOVE_U     3
-#define ACTION_MOVE_D     4
+	ACTION_DASH_L = 5,
+	ACTION_DASH_R = 6,
+	ACTION_DASH_U = 7,
+	ACTION_DASH_D = 8,
 
-// Dash (8 cases, plus cher).
-#define ACTION_DASH_L     5
-#define ACTION_DASH_R     6
-#define ACTION_DASH_U     7
-#define ACTION_DASH_D     8
+	ACTION_TELEPORT_L = 9,
+	ACTION_TELEPORT_R = 10,
+	ACTION_TELEPORT_U = 11,
+	ACTION_TELEPORT_D = 12,
 
-// Teleport (8 cases, cout intermediaire).
-#define ACTION_TELEPORT_L 9
-#define ACTION_TELEPORT_R 10
-#define ACTION_TELEPORT_U 11
-#define ACTION_TELEPORT_D 12
+	ACTION_STILL = 13,
 
-// Ne rien faire ce tour.
-#define ACTION_STILL      13
+	ACTION_BOMB = 14,
+	ACTION_FORK = 15,
+	ACTION_CLEAN = 16,
+	ACTION_MUTE = 17,
+	ACTION_SWAP = 18,
 
-// Actions avancees.
-#define ACTION_BOMB       14
-#define ACTION_FORK       15
-#define ACTION_CLEAN      16
-#define ACTION_MUTE       17
-#define ACTION_SWAP       18
-
-// Nombre total d'actions (borne exclusive).
-#define ACTION_NUMBER     19
+	// Nombre total d'actions (borne exclusive).
+	ACTION_NUMBER = 19
+} Action;
 
 #endif
